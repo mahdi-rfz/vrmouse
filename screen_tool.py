f135fb568f1d9d60e5461fc25ddbb5e802a433f6
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-05-10 13:56:40 +0330

    add help idea

--- screen_tool.py
+++ screen_tool.py
@@ -15,14 +15,16 @@ class Screen_gin :
     #Dividing by two is for find center of the screen
     
     
-    def screen_size():  #output screen size to list
+    def screen_size():
         size = tuple(pygui.size())
         return [size[0] , size[1]]
     
     
-    def try_range( width_try , height_try ): #try screen range like 0 <= x <= 1080
+    def try_range( width_try , height_try ):
         size = tuple(pygui.size())
         if width_try <= size[0] and height_try <= size[1] :
             return True
         else :
             return False
+        
+    # add help method for this script
