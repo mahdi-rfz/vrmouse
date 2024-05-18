20095da4f96eb13c30e7f1121c223552f8be961c
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-05-18 13:44:48 +0330

    block key

--- vrmouse_keyboard_version.py
+++ vrmouse_keyboard_version.py
@@ -2,13 +2,9 @@ import screen_tool
 import keyboard
 import pyautogui as pygui
 import os 
+from pynput import keyboard as pykeyboard
 import time 
 from colorama import Fore
-
-#for later :
-# 1 - change dpi with keys on keyboard 
-# 2 - add additional buttons
-# 3 - add data base
 
 def clear_screen():  #clear screen for widows/linux/mac/maybe my os :)
     if os.name == "nt":
@@ -63,50 +59,52 @@ def keys_input ():
     return [key_list[0] , key_list[1] , key_list[2] , key_list[3] , key_list[4] , key_list[5] , dpi]
 
 
-def main ():
-    #click , rightclick , upkey , downkey , rightkey , leftkey , dpi
-    keys_data = keys_input()
-    clear_screen()
-    
-    print(typography())
-    print(f"{Fore.LIGHTBLACK_EX}Screen size : {Fore.LIGHTGREEN_EX + str(screen_tool.Screen_gin.screen_size())}")
-    print(Fore.WHITE +"________________________________________________")
-    print(Fore.LIGHTBLACK_EX + "Selected keys :")
-    print(f"{Fore.LIGHTBLACK_EX}Click : {Fore.LIGHTGREEN_EX + keys_data[0]}      ,{Fore.LIGHTBLACK_EX}Right Click : {Fore.LIGHTGREEN_EX + keys_data[1]}")
-    print(f"{Fore.LIGHTBLACK_EX}Up : {Fore.LIGHTGREEN_EX + keys_data[2]}      ,{Fore.LIGHTBLACK_EX}Down : {Fore.LIGHTGREEN_EX + keys_data[3]}")
-    print(f"{Fore.LIGHTBLACK_EX}Right : {Fore.LIGHTGREEN_EX + keys_data[4]}      ,{Fore.LIGHTBLACK_EX}Left : {Fore.LIGHTGREEN_EX + keys_data[5]}")
-    print(f"{Fore.LIGHTBLACK_EX}DPI number : {Fore.LIGHTGREEN_EX + str(keys_data[6])}")
-    print(Fore.WHITE + "================================================================")
-    
-    center = screen_tool.Screen_gin.center_finder()
-    i = center[0]
-    j = center[1]
-    
-    dpi = keys_data[6]
-    pygui.moveTo (i , j)
-    
-     #click , rightclick , upkey , downkey , rightkey , leftkey , dpi
-    while True:
-        received_key = keyboard.read_key()
-        
-        if received_key == keys_data[0] : #keys_data[0] for click 
-            pygui.click()
-        elif received_key == keys_data[1] :
-            pygui.rightClick()
-            
-        elif received_key == keys_data[2]: #up
-            j = j - dpi
-            
-        elif received_key == keys_data[3]:
-            j = j + dpi
-            
-        elif received_key == keys_data[4]:#right
-            i = i + dpi
-            
-        elif received_key == keys_data[5]:
-            i = i - dpi            
-            
-        pygui.moveTo(i , j) 
 
-        
-main()
+#click , rightclick , upkey , downkey , rightkey , leftkey , dpi
+keys_data = keys_input()
+clear_screen()
+
+
+center = screen_tool.Screen_gin.center_finder()
+i =  center[0]
+j =  center[1]
+
+pygui.moveTo(i , j)
+dpi = keys_data[6]
+
+def on_press(key):
+
+    global i , j
+    try:
+        if key == pykeyboard.Key.up:
+            j = j - dpi
+        elif key == pykeyboard.Key.down:
+            j = j + dpi
+        elif key == pykeyboard.Key.right:
+            
+            i = i + dpi
+        elif key == pykeyboard.Key.left:
+            i = i - dpi 
+        pygui.moveTo(i , j) 
+        if key ==  x:
+            pygui.click()
+        elif key == pykeyboard.Key.name("shift_r"):
+            pygui.rightClick()
+    except AttributeError:
+        pass
+    
+
+def on_release(key):
+    if key == pykeyboard.Key.esc:
+        # Stop listener
+        return False
+
+
+keyboard.block_key('up')
+keyboard.block_key('down')
+keyboard.block_key('right')
+keyboard.block_key('left')
+
+with pykeyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+    
