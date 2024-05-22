33477fb81739b08618005021b218c880aca50889
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-05-22 21:01:34 +0330

    add custom key for exit

--- vrmouse_keyboard_version.py
+++ vrmouse_keyboard_version.py
@@ -43,9 +43,9 @@ def input_keys_function ():
     
     input_counter = 0
     keys_received_list = []
-    key_function = ["Click" , "RightClick"]
-    default_key_name = ["Enter" , "Right Shift"]
-    key_sign = ["C" , "RC"]
+    key_function = ["Click" , "RightClick" , "Exit"]
+    default_key_name = ["Enter" , "Right Shift" , "ESC"]
+    key_sign = ["C" , "RC" , "E"]
     while True:
         print(Fore.WHITE + f"{Fore.GREEN}[{key_sign[input_counter]}]{Fore.WHITE} Press the key you want for  {key_function[input_counter]} (press Enter for {default_key_name[input_counter]} key) : ")
         input_keys = keyboard.read_key()                  #I repeated this twice because of a problem that the keyboard library 
@@ -53,11 +53,12 @@ def input_keys_function ():
         print(Fore.LIGHTBLACK_EX + "================================================================")
         keys_received_list.append(input_keys)
         input_counter = input_counter + 1
-        if input_counter == 2:
+        if input_counter == 3:
             break
 
     input()
     print(Fore.LIGHTBLACK_EX + ":::::::::( Press Enter ):::::::::")
+    input()
     input()
     
     while True :
@@ -86,8 +87,8 @@ def input_keys_function ():
     block_keys_list = ["up" , "down" , "right" , "left"]
 
 
-    #cshow = click show           rcshow = right click show 
-    global cshow , rcshow
+    #cshow = click show           rcshow = right click show         eshow = exit show 
+    global cshow , rcshow , eshow
     
     #all conditions for click     
     try :
@@ -135,11 +136,36 @@ def input_keys_function ():
             keys_received_list[1] = pykeyboard.Key.ctrl_r
             rcshow = ("RIGHT CTRL")
     
-    return [keys_received_list[0] , keys_received_list[1] , pykeyboard.Key.up , pykeyboard.Key.down , pykeyboard.Key.right , pykeyboard.Key.left , dpi]
+    
+    
+      #all conditions for exit     
+    try :                                          
+        if keys_received_list[2] == "right ctrl" :
+            keys_received_list[2] = pykeyboard.Key.ctrl_r
+            eshow = ("RIGHT CTRL")
+        if keys_received_list[2] == "ctrl" :
+            keys_received_list[2] = pykeyboard.Key.ctrl_l
+            eshow = ("CTRL")
+        if keys_received_list[2] == "right alt" :
+            keys_received_list[2] = pykeyboard.Key.alt_r
+            eshow = ("RIGHT ALT")
+        if keys_received_list[2] == "alt" :
+            keys_received_list[2] = pykeyboard.Key.alt_l
+            eshow = ("ALT")
+        if keys_received_list[2] == "enter" :
+            keys_received_list[2] = pykeyboard.Key.esc
+            eshow = ("ESC")
+        e = eshow
+    except NameError :
+            keys_received_list[2] = pykeyboard.Key.esc
+            eshow = ("ESC")
+    
+    
+    return [keys_received_list[0] , keys_received_list[1] ,keys_received_list[2], pykeyboard.Key.up , pykeyboard.Key.down , pykeyboard.Key.right , pykeyboard.Key.left , dpi]
 
 
 #----------------------------------------------------main-----------------------------------------------------
-#click , rightclick , upkey , downkey , rightkey , leftkey , dpi
+#click , rightclick , exit , upkey , downkey , rightkey , leftkey , dpi
 
 
 input_keys_ftf = input_keys_function()  #input keys ftf = input keys from the function
@@ -148,13 +174,13 @@ clear_screen()
 
 print(typography())
 print(f"{Fore.LIGHTBLACK_EX}Screen size : {Fore.LIGHTGREEN_EX + str(screen_tool.Screen_gin.screen_size())}")
-print(Fore.LIGHTBLACK_EX + "Exit : " , Fore.LIGHTGREEN_EX +" ESC")
+print(Fore.LIGHTBLACK_EX + "Exit : " , Fore.LIGHTGREEN_EX + eshow)
 print(Fore.WHITE +"________________________________________________")
 print(Fore.LIGHTBLACK_EX + "Selected keys :")
 print(f"{Fore.LIGHTBLACK_EX}Click : {Fore.LIGHTGREEN_EX + cshow}      {Fore.LIGHTBLACK_EX}Right Click : {Fore.LIGHTGREEN_EX + rcshow}")
 print(f"{Fore.LIGHTBLACK_EX}Up : {Fore.LIGHTGREEN_EX }UP Key      {Fore.LIGHTBLACK_EX}Down : {Fore.LIGHTGREEN_EX }DOWN Key")
 print(f"{Fore.LIGHTBLACK_EX}Right : {Fore.LIGHTGREEN_EX} RIGHT Key      {Fore.LIGHTBLACK_EX}Left : {Fore.LIGHTGREEN_EX}LEFT Key")
-print(f"{Fore.LIGHTBLACK_EX}DPI number : {Fore.LIGHTGREEN_EX + str(input_keys_ftf[6])}")
+print(f"{Fore.LIGHTBLACK_EX}DPI number : {Fore.LIGHTGREEN_EX + str(input_keys_ftf[7])}")
 print(Fore.WHITE + "================================================================")
 print(Fore.CYAN + "             Coding by mahdi-rfz")
 print(Fore.LIGHTCYAN_EX + "   Contact us : https://github.com/mahdi-rfz")
@@ -180,13 +206,13 @@ def on_press(key):
 
     global i , j
     try:
-        if key == input_keys_ftf[2]:
+        if key == input_keys_ftf[3]:
             j = j - dpi
-        elif key == input_keys_ftf[3]:
-            j = j + dpi
         elif key == input_keys_ftf[4]:
-            i = i + dpi
+            j = j + dpi
         elif key == input_keys_ftf[5]:
+            i = i + dpi
+        elif key == input_keys_ftf[6]:
             i = i - dpi 
         pygui.moveTo(i , j) 
         if key ==  input_keys_ftf[0]:
@@ -199,7 +225,7 @@ def on_press(key):
     
 
 def on_release(key):
-    if key == pykeyboard.Key.esc:
+    if key == input_keys_ftf[2]:
         # Stop listener
         return False
 
